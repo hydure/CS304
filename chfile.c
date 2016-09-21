3c76c2c42cf066b9e8b91f78d169f9559a0c6034
Colin Lightfoot <cflightfoot@gmail.com>
2016-09-20 20:12:47 -0400

    Add files via upload

---
+++ chfile.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  FILE *ptr1_file, *ptr2_file;
+  char buf[1000];
+  int num;
+  char str[100];
+
+  ptr1_file = fopen(argv[1], "r+");
+  if (!ptr1_file){
+    return 1;
+  }
+
+  ptr2_file = fopen(argv[2], "w");
+  if (!ptr2_file){
+    return 1;
+  }
+
+  while (fgets(buf, 1000, ptr1_file)!=NULL){
+    sscanf(buf, "%d %[^\n]s", &num, str);
+    fprintf(ptr2_file, "%d %s\n", num + 1, str);
+  }
+
+    fclose(ptr1_file);
+    fclose(ptr2_file);
+
+    return 0;
+}
